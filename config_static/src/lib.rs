8ed395a30aa4326bbdd4d938a5c30ee989293b10
Chris <chris.clark@coffeeb4code.com>
2022-12-18 10:41:48 -0600

    now need to loop through and work on hard parts

--- config_static/src/lib.rs
+++ config_static/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro2::TokenStream;
 use quote::quote;
 use serde_json::{from_str, Value};
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, Type};
 
 #[proc_macro_attribute]
 pub fn config_static_check(
@@ -26,16 +26,46 @@ pub fn config_static_check(
             .fields
             .iter()
             .map(|field| {
-                let field_name = &field.ident;
-                let u_field = field_name.clone().unwrap().to_string();
+                let field_name = &field.ident.clone().unwrap();
                 let u_json = json
-                    .get(u_field.clone())
-                    .expect(&*format!("missing json value {u_field}"))
-                    .as_str()
-                    .unwrap();
-                let version = String::from(u_json);
-                quote! {
-                    #field_name: #version,
+                    .get(&field_name.to_string())
+                    .expect(&*format!("missing json value {}", field_name.to_string()));
+                // the ultimate intent is to verify that the config file provided is completely
+                // serializable at compile time, and no fields in the Config Struct are missing
+                // from the config file. (right now lazy static rereads the data, and deserializes
+                // on startup to be lazy. later we can fill the object at compile time.
+                match &field.ty {
+                    // only path is hit
+                    Type::Array(typed_arr) => {
+                        println!("sized array");
+                        if u_json.is_array() {
+                        } else {
+                            panic!("{:?} is not a correctly sized array", field_name);
+                        }
+                    }
+                    // only path is hit
+                    Type::Slice(typed_arr) => {
+                        println!("dynamic array");
+                        if !u_json.is_array() {
+                            panic!(
+                                "{:?} is not a correctly dynamically sized array",
+                                field_name
+                            );
+                        }
+                    }
+                    // only this is hit, so we rely on the array being cast correctly for now.
+                    Type::Path(path) => {
+                        println!("path");
+                        if path.path.is_ident("String") {
+                            if !u_json.is_string() {
+                                panic!("{:?} is not a string", field_name);
+                            }
+                        }
+                    }
+
+                    _ => {
+                        panic!("{:?} is not a supported json castable item", field_name);
+                    }
                 }
             })
             .collect::<Vec<_>>(),
--- tester/src/main.rs
+++ tester/src/main.rs
@@ -2,12 +2,16 @@ use config_static::*;
 use serde::Deserialize;
 use serde::Serialize;
 
+// need to support Strings, Arrays, Other Deserializable structs { objects in json terms } (hard to do), usize
+// isize or size whatever is the rust type for signed usize.
 #[config_static_check("config.json")]
 #[derive(Deserialize, Serialize)]
 pub struct Config {
     field: String,
+    vals: Vec<String>,
+    fails: Vec<String>,
 }
 
 fn main() {
-    println!("Hello, world! {}", get_config().field);
+    println!("Hello, world! {:?}", get_config().vals);
 }
