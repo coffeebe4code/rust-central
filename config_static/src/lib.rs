ec37a28aff066add9736ba6850412a7e3acdda56
Chris <chris.clark@coffeeb4code.com>
2022-12-23 15:18:23 -0600

    impler

--- config_static/src/lib.rs
+++ config_static/src/lib.rs
@@ -4,7 +4,7 @@ use serde_json::{from_str, Value};
 use syn::{parse_macro_input, DeriveInput, Type};
 
 #[proc_macro_attribute]
-pub fn config_static_check(
+pub fn config_static_json(
     attr: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
@@ -20,52 +20,40 @@ pub fn config_static_check(
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
-    match input.data {
+    let assertions: Vec<TokenStream> = match input.data {
         // If the input is a struct, loop through its fields
         syn::Data::Struct(ref data) => data
             .fields
             .iter()
             .map(|field| {
                 let field_name = &field.ident.clone().unwrap();
+                let field_type = &field.ty;
                 let u_json = json
                     .get(&field_name.to_string())
                     .expect(&*format!("missing json value {}", field_name.to_string()));
-                // the ultimate intent is to verify that the config file provided is completely
-                // serializable at compile time, and no fields in the Config Struct are missing
-                // from the config file. (right now lazy static rereads the data, and deserializes
-                // on startup to be lazy. later we can fill the object at compile time.
-                match &field.ty {
-                    // only path is hit
-                    Type::Array(typed_arr) => {
-                        println!("sized array");
-                        if u_json.is_array() {
-                        } else {
-                            panic!("{:?} is not a correctly sized array", field_name);
-                        }
-                    }
-                    // only path is hit
-                    Type::Slice(typed_arr) => {
-                        println!("dynamic array");
-                        if !u_json.is_array() {
-                            panic!(
-                                "{:?} is not a correctly dynamically sized array",
-                                field_name
-                            );
-                        }
-                    }
-                    // only this is hit, so we rely on the array being cast correctly for now.
-                    Type::Path(path) => {
-                        println!("path");
-                        if path.path.is_ident("String") {
-                            if !u_json.is_string() {
-                                panic!("{:?} is not a string", field_name);
-                            }
-                        }
-                    }
-
-                    _ => {
-                        panic!("{:?} is not a supported json castable item", field_name);
-                    }
+                if u_json.is_string() {
+                    return quote! {
+                        const fn assert_string<T: IsString>() {}
+                        const _: () = {
+                            assert_string::<#field_type>();
+                        };
+                    };
+                } else if u_json.is_boolean() {
+                    return quote! {
+                        const fn assert_bool<T: IsBoolean>() {}
+                        const _: () = {
+                            assert_bool::<#field_type>();
+                        };
+                    };
+                } else if u_json.is_i64() {
+                    return quote! {
+                        const fn assert_i64<T: IsInt64>() {}
+                        const _: () = {
+                            assert_i64::<#field_type>();
+                        };
+                    };
+                } else {
+                    panic!("not a supported json field {:?}", field_name);
                 }
             })
             .collect::<Vec<_>>(),
@@ -75,6 +63,13 @@ pub fn config_static_check(
     let expanded = quote! {
         use serde_json::from_str;
         use once_cell::sync::Lazy;
+
+        trait IsString {}
+        trait IsInt64 {}
+        trait IsBoolean {}
+        impl IsString for String {}
+        impl IsInt64 for i64 {}
+        impl IsBoolean for bool {}
 
         static INSTANCE: Lazy<#name> = Lazy::new(|| {
             let file_contents = std::fs::read_to_string(#file_path).unwrap();
@@ -86,6 +81,7 @@ pub fn config_static_check(
             &INSTANCE
         }
         #input
+        #(#assertions)*
     };
 
     proc_macro::TokenStream::from(expanded)
--- tester/src/main.rs
+++ tester/src/main.rs
@@ -2,16 +2,16 @@ use config_static::*;
 use serde::Deserialize;
 use serde::Serialize;
 
-// need to support Strings, Arrays, Other Deserializable structs { objects in json terms } (hard to do), usize
-// isize or size whatever is the rust type for signed usize.
-#[config_static_check("config.json")]
+#[config_static_json("config.json")]
 #[derive(Deserialize, Serialize)]
 pub struct Config {
     field: String,
-    vals: Vec<String>,
-    fails: Vec<String>,
+    booly: bool,
+    big_int: i64,
 }
 
+pub struct KakfaConfig {}
+
 fn main() {
-    println!("Hello, world! {:?}", get_config().vals);
+    println!("Hello, world! {:?}", get_config().booly.to_string());
 }
